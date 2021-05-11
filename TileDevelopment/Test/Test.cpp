230ac5cde1c46355cb0ed5c022d176bc33d621ad
striderssoftware <strider@shadowsoftware.biz>
2021-05-11 09:24:58 -0600

    + Update the SDL Test POP.

--- TileDevelopment/Test/Test.cpp
+++ TileDevelopment/Test/Test.cpp
@@ -23,7 +23,7 @@ int main()
     }
 
   //do something video
-  SDL_Window* window = SDL_CreateWindow("woo woo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+  SDL_Window* window = SDL_CreateWindow("say bow wow for yogie", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1640, 1280, SDL_WINDOW_SHOWN);
 
   if ( window == nullptr )
     {
@@ -32,9 +32,30 @@ int main()
     }
 
 
-  SDL_UpdateWindowSurface(window);
-  SDL_Delay(2000);
+  SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
+  // Select the color for drawing. It is set to red here.
+  //SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+  
+  // Clear the entire screen to our selected color.
+  SDL_RenderClear(renderer);
+
+  SDL_Surface* image = SDL_LoadBMP("./MagicTheature.bmp");
+  SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, image);
+  SDL_RenderCopy(renderer, texture, NULL, NULL);
+  
+  // Up until now everything was drawn behind the scenes.
+  // This will show the new, red contents of the window.
+  SDL_RenderPresent(renderer);
+  
+
+  SDL_UpdateWindowSurface(window);
+  SDL_Delay(9000);
+
+
+  SDL_DestroyTexture(texture);
+  SDL_FreeSurface(image);
+  SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
   SDL_Quit();
 }
